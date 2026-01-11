1772dc37b3159a6c4a02e80983c3889ebfc3fb95
23-uso <2022m019@hokuriku-u.ac.jp>
2026-01-12 00:06:14 +0900

    Add Render host

--- devProject/settings.py
+++ devProject/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True' 
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testproject-fwt6.onrender.com']
 
 
 # Application definition
